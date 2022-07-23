b87a085a649986474956993f90d8d55e1414b902
piotrpoleszak <poleszak@protonmail.com>
2022-07-23 23:20:14 +0200

    Implement Eureka clients

--- fraud/src/main/java/com/poleszak/fraud/FraudApplication.java
+++ fraud/src/main/java/com/poleszak/fraud/FraudApplication.java
@@ -2,9 +2,11 @@ package com.poleszak.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @SuppressWarnings("unused")
+@EnableEurekaClient
 public class FraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);

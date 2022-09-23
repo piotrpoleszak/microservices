9740b2d10b96e37dc3a6e0b9ca80e77ffe4baa3b
piotrpoleszak <poleszak@protonmail.com>
2022-09-24 00:54:05 +0200

    Refactor Feign Clients and add spring profiles

--- fraud/src/main/java/com/poleszak/fraud/FraudApplication.java
+++ fraud/src/main/java/com/poleszak/fraud/FraudApplication.java
@@ -3,10 +3,15 @@ package com.poleszak.fraud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SuppressWarnings("unused")
 @SpringBootApplication
 @EnableEurekaClient
+@PropertySources({
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+})
 public class FraudApplication {
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);
